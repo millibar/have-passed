204ab35bf6d22d691300edde91451c9bb60d1f04
millibar <millibarjp@gmail.com>
2023-08-20 09:33:03 +0900

    PWAにautoUpdateを追加

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      registerType: 'autoUpdate',
       manifest: {
         id: '/have-passed/',
         name: 'have passed',
